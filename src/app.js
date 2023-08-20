15bcfb70b6777800d10eec94d3430c970e3bef67
Martiniano <bianchimartiniano@gmail.com>
2023-08-20 19:37:33 -0300

    cambio de ruta

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.listen(PORT, () => {
 })
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/home.html'))
+  res.sendFile(path.join(__dirname, 'views/index.html'))
 })
 app.get('/carrito.html', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/carrito.html'))
