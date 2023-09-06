446519873cb5902341a277704adde1c2a778d094
sofia-chiodi <chiodi.sb@gmail.com>
2023-09-05 21:54:31 -0300

    En la weekly se corrige ruta de add-form

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,12 +1,12 @@
-const express = require('express')
+const express = require('express');
 
-const productsRouter = express.Router()
-const productsController = require('../controllers/productsController')
+const productsRouter = express.Router();
+const productsController = require('../controllers/productsController');
 
-productsRouter.get('/cart', productsController.productCart)
-productsRouter.get('/cart-filled', productsController.productCartFilled)
-productsRouter.get('/detail', productsController.productDetail)
-productsRouter.get('/list', productsController.productList)
-productsRouter.get('/list', productsController.productAddForm)
+productsRouter.get('/cart', productsController.productCart);
+productsRouter.get('/cart-filled', productsController.productCartFilled);
+productsRouter.get('/detail', productsController.productDetail);
+productsRouter.get('/list', productsController.productList);
+productsRouter.get('/add-form', productsController.productAddForm);
 
-module.exports = productsRouter
+module.exports = productsRouter;
