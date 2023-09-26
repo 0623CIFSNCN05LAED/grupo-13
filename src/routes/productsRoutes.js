3ab7b7bf9292b7d7e2f8597729a9ddd5561f0fd8
JuliMa23 <julianamoncada41@gmail.com>
2023-09-26 00:17:21 -0500

    Se implementa multer y sus métodos

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,6 +1,22 @@
 const express = require('express'); // requerir el paquete express
 const productsRouter = express.Router(); // almacenando en una variable las funciones de rutas (get, post, put, delete)
+const multer = require('multer');
+const path = require('path');
 const productsController = require('../controllers/productsController'); // vincula el router con el controlador
+
+const storage= multer.diskStorage({
+    destination: path.join(__dirname, '../../public/images/products'),
+    filename: function(req, file, cb){
+        cb( null, file.fieldname + '-' + Date.now() + path.extname(file.originalname) 
+        );
+    }
+
+});
+
+const upload= multer({
+    storage: storage
+});
+
 
 /* PRODUCT LIST */
 productsRouter.get('/', productsController.index);
@@ -14,14 +30,14 @@ productsRouter.delete('/:id/delete', productsController.destroy); // EDIT-FORM u
 
 /* PRODUCT ADD FORM */
 productsRouter.get('/create', productsController.addForm); // muestra el formulario ADD PRODUCT
-productsRouter.post('/create', productsController.store); // URL que contiene la info del producto creado con ADD FORM al enviar
+productsRouter.post('/create', upload.single('productPicture'), productsController.store); // URL que contiene la info del producto creado con ADD FORM al enviar
 
 /* PRODUCT DETAIL */
 productsRouter.get('/:id', productsController.detail); // Segun el ID ingresado la URL
 
 /* PRODUCT EDIT FORM */
 productsRouter.get('/:id/edit', productsController.editForm); // muestra el formulario EDIT-FORM segun el ID
-productsRouter.put('/:id/edit', productsController.update); // url que va a contener la info enviada por el formulario de EDIT PRODUCT al presionar enviar
+productsRouter.put('/:id/edit',  upload.single('productPicture'), productsController.update); // url que va a contener la info enviada por el formulario de EDIT PRODUCT al presionar enviar
 
 // productsRouter.put('/detail/:id', productsController.productEditPut);
 
