7a0419a8da7ab6276c68031a445f1d59632bc6d9
florenciamartinmersing <fmartinmersing@gmail.com>
2023-10-01 21:47:17 -0300

    creo metodo post de users

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -3,10 +3,11 @@ const express = require('express');
 const usersRouter = express.Router();
 const usersController = require('../controllers/usersController');
 
-usersRouter.get('/login', usersController.login)
-usersRouter.get('/register', usersController.register)
-usersRouter.get('/myProfile', usersController.myProfile)
-usersRouter.get('/myProfileAdmin', usersController.myProfileAdmin)
+usersRouter.get('/login', usersController.login);
+usersRouter.get('/register', usersController.registerForm);
+usersRouter.post('/register', usersController.register);
 
+usersRouter.get('/myProfile', usersController.myProfile);
+usersRouter.get('/myProfileAdmin', usersController.myProfileAdmin);
 
 module.exports = usersRouter;
