079a41694d81b0b10d6ddead28cd3750735246f7
Maria Rodriguez <mariavardiles@gmail.com>
2023-10-08 18:30:14 -0300

    resuelvo issue #55

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -4,19 +4,19 @@ const multer = require('multer');
 const path = require('path');
 const productsController = require('../controllers/productsController'); // vincula el router con el controlador
 
-const storage= multer.diskStorage({
-    destination: path.join(__dirname, '../../public/images/products'),
-    filename: function(req, file, cb){
-        cb( null, file.fieldname + '-' + Date.now() + path.extname(file.originalname) 
-        );
-    }
-
+const storage = multer.diskStorage({
+  destination: path.join(__dirname, '../../public/images/products'),
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + '-' + Date.now() + path.extname(file.originalname)
+    );
+  },
 });
 
-const upload= multer({
-    storage: storage
+const upload = multer({
+  storage: storage,
 });
-
 
 /* PRODUCT LIST */
 productsRouter.get('/', productsController.index);
@@ -24,25 +24,33 @@ productsRouter.get('/', productsController.index);
 /* CRUD */
 productsRouter.get('/crud', productsController.productCrud); // tabla de edicion de productos cuando se ingresa con perfil admin
 
+/* PRODUCT ADD FORM */
+productsRouter.get('/create', productsController.addForm); // muestra el formulario ADD PRODUCT
+productsRouter.post(
+  '/create',
+  upload.single('productPicture'),
+  productsController.store
+); // URL que contiene la info del producto creado con ADD FORM al enviar
+
+/* PRODUCT CART */
+productsRouter.get('/cart', productsController.productCart);
+productsRouter.get('/cart-filled', productsController.productCartFilled);
+
 /* PRODUCT DELETE FORM */
 productsRouter.get('/:id/delete', productsController.deleteForm); // muestra el formulario DELETE PRODUCT
 productsRouter.delete('/:id/delete', productsController.destroy); // EDIT-FORM url que contiene la info enviada por el formulario de edit product al eliminar
-
-/* PRODUCT ADD FORM */
-productsRouter.get('/create', productsController.addForm); // muestra el formulario ADD PRODUCT
-productsRouter.post('/create', upload.single('productPicture'), productsController.store); // URL que contiene la info del producto creado con ADD FORM al enviar
 
 /* PRODUCT DETAIL */
 productsRouter.get('/:id', productsController.detail); // Segun el ID ingresado la URL
 
 /* PRODUCT EDIT FORM */
 productsRouter.get('/:id/edit', productsController.editForm); // muestra el formulario EDIT-FORM segun el ID
-productsRouter.put('/:id/edit',  upload.single('productPicture'), productsController.update); // url que va a contener la info enviada por el formulario de EDIT PRODUCT al presionar enviar
+productsRouter.put(
+  '/:id/edit',
+  upload.single('productPicture'),
+  productsController.update
+); // url que va a contener la info enviada por el formulario de EDIT PRODUCT al presionar enviar
 
 // productsRouter.put('/detail/:id', productsController.productEditPut);
-
-productsRouter.get('/cart', productsController.productCart);
-productsRouter.get('/cart-filled', productsController.productCartFilled);
-productsRouter.get('/product-form', productsController.productForm); // pagina de validacion para ingresar a edit form o al add form
 
 module.exports = productsRouter;
