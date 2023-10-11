d4e4203135b478bb1853f4afca749c5654ebcfdb
sofia-chiodi <chiodi.sb@gmail.com>
2023-10-10 21:49:41 -0300

    Se corrige profile-edit y users.json

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const usersRouter = express.Router()
-const multer = require('multer')
-const path = require('path')
-const usersController = require('../controllers/usersController')
+const express = require('express');
+const usersRouter = express.Router();
+const multer = require('multer');
+const path = require('path');
+const usersController = require('../controllers/usersController');
 
 const storage = multer.diskStorage({
   destination: path.join(__dirname, '../../public/images/users'),
@@ -10,30 +10,34 @@ const storage = multer.diskStorage({
     cb(
       null,
       file.fieldname + '-' + Date.now() + path.extname(file.originalname)
-    )
+    );
   },
-})
+});
 
 const upload = multer({
   storage: storage,
-})
+});
 
-usersRouter.get('/login', usersController.login)
-usersRouter.get('/register', usersController.registerForm)
+usersRouter.get('/login', usersController.login);
+usersRouter.get('/register', usersController.registerForm);
 usersRouter.post(
   '/register',
   upload.single('userPicture'),
   usersController.register
-)
+);
 
-usersRouter.get('/crud', usersController.crud)
+usersRouter.get('/crud', usersController.crud);
 
-usersRouter.get('/:id/delete', usersController.deleteForm)
-usersRouter.delete('/:id/delete', usersController.destroy)
+usersRouter.get('/:id/delete', usersController.deleteForm);
+usersRouter.delete('/:id/delete', usersController.destroy);
 
-usersRouter.get('/:id/edit', usersController.myProfileEdit)
-usersRouter.put('/:id/edit', usersController.update)
+usersRouter.get('/:id/edit', usersController.myProfileEdit);
+usersRouter.put(
+  '/:id/edit',
+  upload.single('userPicture'),
+  usersController.update
+);
 
-usersRouter.get('/:id', usersController.myProfile)
+usersRouter.get('/:id', usersController.myProfile);
 
-module.exports = usersRouter
+module.exports = usersRouter;
