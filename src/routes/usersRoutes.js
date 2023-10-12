3da8327899d334e90d65ef301f5db1761b6bfe73
clemensolana <clementesolana@outlook.com>
2023-10-12 00:10:22 -0300

    Sofia y Clemente corrigen errores con multer, se agrega la posibilidad de editar imagenes

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const usersRouter = express.Router()
-const multer = require('multer')
-const path = require('path')
-const usersController = require('../controllers/usersController')
+const express = require('express');
+const usersRouter = express.Router();
+const multer = require('multer');
+const path = require('path');
+const usersController = require('../controllers/usersController');
 
 const storage = multer.diskStorage({
   destination: path.join(__dirname, '../../public/images/users'),
@@ -10,30 +10,32 @@ const storage = multer.diskStorage({
     cb(
       null,
       file.fieldname + '-' + Date.now() + path.extname(file.originalname)
-    )
+    );
   },
-})
+});
 
 const upload = multer({
   storage: storage,
-})
+});
 
-usersRouter.get('/login', usersController.login)
-usersRouter.get('/register', usersController.registerForm)
+usersRouter.get('/login', usersController.login);
+usersRouter.get('/register', usersController.registerForm);
 usersRouter.post(
   '/register',
   upload.single('userPicture'),
   usersController.register
-)
+);
 
-usersRouter.get('/crud', usersController.crud)
+usersRouter.get('/crud', usersController.crud);
 
-usersRouter.get('/:id/delete', usersController.deleteForm)
-usersRouter.delete('/:id/delete', usersController.destroy)
+usersRouter.get('/:id/delete', usersController.deleteForm);
+usersRouter.delete('/:id/delete', usersController.destroy);
 
-usersRouter.get('/:id/edit', usersController.myProfileEdit)
-usersRouter.put('/:id/edit', usersController.update)
+usersRouter.get('/:id/edit', usersController.myProfileEdit);
+// usersRouter.get('/:id/prueba', usersController.myProfileEdit); futuro editor admin
+// usersRouter.put('/:id', upload.single('image'), usersController.update); futuro editor admin
+usersRouter.put('/:id', upload.single('image'), usersController.update);
 
-usersRouter.get('/:id', usersController.myProfile)
+usersRouter.get('/:id', usersController.myProfile);
 
-module.exports = usersRouter
+module.exports = usersRouter;
