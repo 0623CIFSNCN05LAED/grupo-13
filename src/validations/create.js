c38845aad165296d630e7c140a3557cd8b1a13c1
sofia-chiodi <chiodi.sb@gmail.com>
2023-10-26 00:26:36 -0300

    Actualizo mensajes de error en create validations

--- src/validations/create.js
+++ src/validations/create.js
@@ -1,28 +1,26 @@
 const { body } = require('express-validator');
 
 module.exports = [
-  body('firstName').notEmpty().withMessage('Ingresá tu nombre'),
-  body('lastName').notEmpty().withMessage('Ingresá tu apellido'),
+  body('firstName').notEmpty().withMessage('Ingresar nombre'),
+  body('lastName').notEmpty().withMessage('Ingresar apellido'),
   body('email')
     .notEmpty()
-    .withMessage('Ingresá tu correo electrónico')
+    .withMessage('Ingresar correo electrónico')
     .bail()
     .isEmail()
-    .withMessage('Ingresá un correo electrónico válido'),
+    .withMessage('Ingresar un correo electrónico válido'),
   body('password')
     .notEmpty()
-    .withMessage('Ingresá tu contraseña')
+    .withMessage('Ingresar contraseña provisoria')
     .bail()
     .isLength({ min: 8 })
     .withMessage('La contraseña debe tener al menos 8 caracteres'),
   body('contactNumber')
     .notEmpty()
-    .withMessage('Ingresá tu número de teléfono')
+    .withMessage('Ingresar número de teléfono')
     .bail()
     .isInt()
-    .withMessage(
-      'Ingresá un número de teléfono válido (sin guiones, ni espacios)'
-    ),
-  body('birthDate').notEmpty().withMessage('Ingresá tu fecha de nacimiento'),
-  body('address').notEmpty().withMessage('Ingresá tu dirección'),
+    .withMessage('Ingresar un teléfono válido (sin caracteres, ni espacios)'),
+  body('birthDate').notEmpty().withMessage('Ingresar fecha de nacimiento'),
+  body('address').notEmpty().withMessage('Ingresar dirección'),
 ];
