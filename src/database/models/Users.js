7f84db9069fdb80f5b7bb38081bd2a508a0612ee
clemensolana <clementesolana@outlook.com>
2023-11-14 21:06:47 -0300

    Se crea el modelo de Users

--- src/database/models/Users.js
+++ src/database/models/Users.js
@@ -1,4 +1,43 @@
 module.exports = (sequelize, dataTypes) => {
+  const alias = 'Users';
+  const cols = {
+    id: {
+      type: dataTypes.STRING,
+      primaryKey: true,
+    },
+    firstName: {
+      type: dataTypes.STRING,
+    },
+    lastName: {
+      type: dataTypes.STRING,
+    },
+    email: {
+      type: dataTypes.STRING,
+    },
+    password: {
+      type: dataTypes.STRING,
+    },
+    contacNumber: {
+      type: dataTypes.INTENGER,
+    },
+    birthDate: {
+      type: dataTypes.DATE,
+    },
+    address: {
+      type: dataTypes.STRING,
+    },
+    profilePicture: {
+      type: dataTypes.STRING,
+    },
+    accessType: {
+      type: dataTypes.STRING,
+    },
+  };
+  const config = {
+    tableName: 'Users',
+    timestamps: false,
+  };
+
   const Users = sequelize.define(alias, cols, config);
 
   return Users;
