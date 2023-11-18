0d6980043f803e90ffdbe71b0ed0b520b7c80fed
sofia-chiodi <chiodi.sb@gmail.com>
2023-11-18 19:31:25 -0300

    Actualizo modelos

--- src/database/models/Users.js
+++ src/database/models/Users.js
@@ -1,44 +1,44 @@
 module.exports = (sequelize, DataTypes) => {
-  const alias = 'users';
-  const cols = {
-    id: {
-      type: DataTypes.STRING,
-      primaryKey: true,
+  const Model = sequelize.define(
+    'Users',
+    {
+      id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+      },
+      firstName: {
+        type: DataTypes.STRING,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+      },
+      email: {
+        type: DataTypes.STRING,
+      },
+      password: {
+        type: DataTypes.STRING,
+      },
+      contactNumber: {
+        type: DataTypes.INTEGER,
+      },
+      birthDate: {
+        type: DataTypes.DATE,
+      },
+      address: {
+        type: DataTypes.STRING,
+      },
+      profilePicture: {
+        type: DataTypes.STRING,
+      },
+      accessType: {
+        type: DataTypes.STRING,
+      },
     },
-    firstName: {
-      type: DataTypes.STRING,
-    },
-    lastName: {
-      type: DataTypes.STRING,
-    },
-    email: {
-      type: DataTypes.STRING,
-    },
-    password: {
-      type: DataTypes.STRING,
-    },
-    contactNumber: {
-      type: DataTypes.INTEGER,
-    },
-    birthDate: {
-      type: DataTypes.DATE,
-    },
-    address: {
-      type: DataTypes.STRING,
-    },
-    profilePicture: {
-      type: DataTypes.STRING,
-    },
-    accessType: {
-      type: DataTypes.STRING,
-    },
-  };
-  const config = {
-    tableName: 'users',
-    timestamps: false,
-  };
-
-  const Users = sequelize.define(alias, cols, config);
+    {
+      tableName: 'users',
+      timestamps: false,
+    }
+  );
 
   return Users;
 };
