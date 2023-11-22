77515049a038085c63b09ef086bda1864ffbc98d
Martiniano <bianchimartiniano@gmail.com>
2023-11-21 22:09:51 -0300

    actualizacion de controladores, servicios, y validaciones de users

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,76 +1,56 @@
-const { Users } = require('../database/models');
-const { v4: uuidv4 } = require('uuid');
-const Sequelize = require('sequelize');
+const { Users } = require('../database/models')
+const { v4: uuidv4 } = require('uuid')
+const bcryptjs = require('bcryptjs')
 
 const userServices = {
   getAllUsers: () => {
-    return Users.findAll();
+    return Users.findAll()
   },
   getUser: async (id) => {
-    return await Users.findByPk(id);
+    return await Users.findByPk(id)
   },
   getUserByEmail: async (query) => {
     const user = await Users.findOne({
       where: {
         email: query,
       },
-    });
-    return user;
+    })
+    return user
   },
   createUser: (body, file) => {
-    console.log('Creando usuario');
     return Users.create({
       id: uuidv4(),
       first_name: body.first_name,
       last_name: body.last_name,
       email: body.email,
-      password: body.password,
+      password: bcryptjs.hashSync(body.password, 10),
       contact_number: Number(body.contact_number),
       birth_date: body.birth_date,
       address: body.address,
-      profile_picture: file ? file.filename : 'default-image.png',
+      profile_picture: file == 'null' ? file.filename : 'default-image.png',
       role_id: body.email.includes('@ebeer.com') ? 1 : 2,
-    });
-  },
-  createUserAdmin: (user) => {
-    if (!user.role_id) {
-      user.role_id = 'user';
-    }
-    const newUser = {
-      id: uuidv4(),
-      role_id: user.email.includes('@ebeer.com') ? 'admin' : 'user',
-      ...user,
-    };
-    return Users.create({
-      newUser,
-    });
+    })
   },
   updateUser: async (id, body, file) => {
-    console.log('Updating user');
-    const user = await Users.findByPk(id);
-    const profile_picture = file ? file.filename : user.profile_picture;
+    const user = await Users.findByPk(id)
+
     return Users.update(
       {
-        id: id,
-        first_name: body.first_name,
-        last_name: body.last_name,
         email: body.email,
-        password: body.password,
+        profile_picture: file ? file.filename : user.profile_picture,
         contact_number: Number(body.contact_number),
-        birth_date: body.birth_date,
         address: body.address,
-        profile_picture: profile_picture,
       },
       {
         where: { id: id },
       }
-    );
+    )
   },
   deleteUser: (id) => {
     return Users.destroy({
       where: { id: id },
-    });
+    })
   },
-};
+}
 
-module.exports = userServices;
+module.exports = userServices
--- src/validations/create.js
+++ src/validations/create.js
@@ -1,26 +1,28 @@
-const { body } = require('express-validator');
+const { body } = require('express-validator')
 
 module.exports = [
-  body('firstName').notEmpty().withMessage('Ingresar nombre'),
-  body('lastName').notEmpty().withMessage('Ingresar apellido'),
+  body('first_name').notEmpty().withMessage('Ingresá tu nombre'),
+  body('last_name').notEmpty().withMessage('Ingresá tu apellido'),
   body('email')
     .notEmpty()
-    .withMessage('Ingresar correo electrónico')
+    .withMessage('Ingresá tu correo electrónico')
     .bail()
     .isEmail()
-    .withMessage('Ingresar un correo electrónico válido'),
+    .withMessage('Ingresá un correo electrónico válido'),
   body('password')
     .notEmpty()
-    .withMessage('Ingresar contraseña provisoria')
+    .withMessage('Ingresá tu contraseña')
     .bail()
     .isLength({ min: 8 })
     .withMessage('La contraseña debe tener al menos 8 caracteres'),
-  body('contactNumber')
+  body('contact_number')
     .notEmpty()
-    .withMessage('Ingresar número de teléfono')
+    .withMessage('Ingresá tu número de teléfono')
     .bail()
     .isInt()
-    .withMessage('Ingresar un teléfono válido (sin caracteres, ni espacios)'),
-  body('birthDate').notEmpty().withMessage('Ingresar fecha de nacimiento'),
-  body('address').notEmpty().withMessage('Ingresar dirección'),
-];
+    .withMessage(
+      'Ingresá un número de teléfono válido (sin guiones, ni espacios)'
+    ),
+  body('birth_date').notEmpty().withMessage('Ingresá tu fecha de nacimiento'),
+  body('address').notEmpty().withMessage('Ingresá tu dirección'),
+]
