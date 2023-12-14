1e6f057e4ac597e5965f08a9da54b2c3e41e2ef7
Martiniano <bianchimartiniano@gmail.com>
2023-12-13 23:54:19 -0300

    create react app dashboard

---
+++ dashboard/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import '../css/header.css'
+// import logo from '../../public/logo.png'
+
+export default function Header() {
+  return (
+    <header>
+      <div class="header-container">
+        <div class="nav-icon">
+          <a href="/home">
+            <img src="/images/logo.png" alt="imagen logo" />
+          </a>
+        </div>
+
+        <nav class="nav-container">
+          <ul>
+            <li>
+              <a href="/products">Productos</a>
+            </li>
+            <li>
+              <a href="/users">Usuarios</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
