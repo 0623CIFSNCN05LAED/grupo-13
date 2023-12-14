b9c84abd6b195029571a098fbe411831f550e3a3
Martiniano <bianchimartiniano@gmail.com>
2023-12-13 23:35:22 -0300

    add react app

---
+++ e-beer/src/routes/mainRoutes.js
@@ -0,0 +1,34 @@
+// Require's
+const express = require('express');
+const mainRouter = express.Router();
+const ageAuth = require('../middlewares/ageAuthMiddleware');
+const apiRouter = require('./api/mainRoutes');
+
+// Controller require
+const mainController = require('../controllers/mainController');
+
+// Main routes
+
+//comingAge
+mainRouter.get('/', mainController.comingAgeForm);
+mainRouter.post('/', mainController.comingAge);
+
+mainRouter.get('/home', ageAuth, mainController.index);
+mainRouter.get('/contact', ageAuth, mainController.contact);
+mainRouter.get('/about-us', ageAuth, mainController.aboutUs);
+mainRouter.get('/tyc', mainController.tyc);
+mainRouter.get('/coming-age-no', mainController.comingAgeNo);
+
+// Products routes
+const productsRoutes = require('./productsRoutes.js');
+mainRouter.use('/products', ageAuth, productsRoutes);
+
+// Users routes
+const usersRoutes = require('./usersRoutes.js');
+mainRouter.use('/users', ageAuth, usersRoutes);
+
+// API routes
+
+mainRouter.use('/api', apiRouter);
+
+module.exports = mainRouter;
