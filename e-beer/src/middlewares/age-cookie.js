99c57bd3ba2ec10a5304f81d244e219c93a329a9
clemensolana <clementesolana@outlook.com>
2024-01-07 22:37:17 -0300

    Se actualizan middleware de age-cookie y imagen de perfil en my-profile-update

--- e-beer/src/middlewares/age-cookie.js
+++ e-beer/src/middlewares/age-cookie.js
@@ -1,5 +1,8 @@
 module.exports = (req, res, next) => {
-  if (req.cookies.email != undefined) {
+  if (
+    (req.cookies && req.cookies.email != undefined) ||
+    (req.session.userLogged && req.session.userLogged.email != undefined)
+  ) {
     req.session.ageAnswer = 'yes';
     res.locals.ageAnswer = true;
     res.locals.ageAnswer = req.session.ageAnswer;
